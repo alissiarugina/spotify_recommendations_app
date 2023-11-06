61e93d6e6278881b339f51286f5f1f2942a34c9b
alissiarugina <78981668+alissiarugina@users.noreply.github.com>
2023-11-06 22:49:01 +0000

    change uri in music_recommendations.py

--- music_recommendations.py
+++ music_recommendations.py
@@ -11,7 +11,7 @@ client_secret = ""
 sp_oauth = SpotifyOAuth(
     client_id,
     client_secret,
-    redirect_uri="http://localhost:8888/callback",
+    redirect_uri="http://localhost:8501",
     scope = "user-modify-playback-state"
     )
 
