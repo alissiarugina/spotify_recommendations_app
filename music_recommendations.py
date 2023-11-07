ec9b5b7efdfef6d8ae2d4401140a3dfb5a265dd2
alissiarugina <78981668+alissiarugina@users.noreply.github.com>
2023-11-07 20:32:41 +0000

    localhost:8501

--- music_recommendations.py
+++ music_recommendations.py
@@ -11,7 +11,7 @@ client_secret = ""
 sp_oauth = SpotifyOAuth(
     client_id,
     client_secret,
-    redirect_uri="spotifyrecommendationsapp.streamlit.app",
+    redirect_uri="http://localhost:8501",
     scope = "user-modify-playback-state"
     )
 
